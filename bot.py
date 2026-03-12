ab2d2e12cd14491273302a39c6a14fdb3194299c
demid2005d-web <demid2005d@gmail.com>
2026-03-11 23:46:57 -1200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
 
-# Включаем отладку
-# Временно для проверки
-TOKEN = "8692892233:AAFQ0tuhuj01WJ1ub6_sabpBaU5QyROh-do"
-print(f"DEBUG: Значение TOKEN равно: {TOKEN}") # Это выведет значение в логи
-
-if TOKEN is None:
-    print("ОШИБКА: Переменная BOT_TOKEN не найдена в окружении!")
-    exit(1) # Завершаем работу, если токена нет
+# Сейчас мы используем токен из кода для проверки
+TOKEN = "8692892233:AAF0tuhuj01WJ1ub6_sabpBaU5QyROh-do"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Обработчик команды /start
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("✅ Бот успешно запущен и работает на Railway!")
+
 async def main():
     print("Бот успешно запущен!")
+    # Очистка очереди сообщений
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
